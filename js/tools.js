945183b93e6a62cd5f3e8bce4de3d895f0755d68
AlexanderLislelid <alexlislelid@gmail.com>
2025-12-16 15:20:23 +0100

    Add tools data: create tools.js with initial tool entries and images

---
+++ js/tools.js
@@ -0,0 +1,38 @@
+export const tools = [
+  {
+    id: "vscode",
+    title: "VS Code",
+    description: "insert text here",
+    image: "./images/icons/vscode.svg",
+  },
+  {
+    id: "figma",
+    title: "Figma",
+    description: "insert text here",
+    image: "./images/icons/figma-brands-solid-full.svg",
+  },
+  {
+    id: "git",
+    title: "Git",
+    description: "insert text here",
+    image: "./images/icons/git-alt-brands-solid-full.svg",
+  },
+  {
+    id: "html",
+    title: "html",
+    description: "insert text here",
+    image: "./images/icons/html5-brands-solid-full.svg",
+  },
+  {
+    id: "css",
+    title: "css",
+    description: "insert text here",
+    image: "../images/icons/css-brands-solid-full.svg",
+  },
+  {
+    id: "js",
+    title: "javascript",
+    description: "insert text here",
+    image: "../images/icons/js-brands-solid-full (1).svg",
+  },
+];
